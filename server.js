d43d16884c43035a28c659f3c5127beb6166e3a5
srivenky12 <srivenky23@gmail.com>
2017-08-14 11:06:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ app.get('/article-one', function (req, res) {
 });
 
 app.get('/article-two', function (req, res) {
- res.send('Article two  wil served here');
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 app.get('/article-three', function (req, res) {
-  res.send('Article three  wil served here');
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
