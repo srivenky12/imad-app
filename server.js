a171a794b57161dd5a2f7c35e403f17dd086e7a1
srivenky12 <srivenky23@gmail.com>
2017-08-14 13:37:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ var articles = {
 };
 
 function createTemplate(data) {
-    var title = data.title,
+    var title = data.title;
     var date = data.date;
     var heading = data.heading;
     var content = data.content;
