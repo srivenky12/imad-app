0ec16b79e55ba71c01957f64f21b36d167f73414
srivenky12 <srivenky23@gmail.com>
2017-08-14 17:24:32 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,6 +8,13 @@ element.innerHTML = "New value From Main.js";
 ///move the the image 
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight () {
+    marginLeft = marginLeft + 1;
+    img.style.marginLeft = marginLeft + 'px';
+}
 img.onClick = function(){
-    img.style.marginLeft = '100px';
+    var interval = setInterval(moveRight,50);
+   
+    
 };
