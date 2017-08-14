379d90907b48355803eab63d961c5f0a978441d2
srivenky12 <srivenky23@gmail.com>
2017-08-14 12:50:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-   res.send(createTempalate(articleOne));
+   res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
